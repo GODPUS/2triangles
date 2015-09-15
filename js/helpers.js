454de9f9e1ab39413badaeb403d220194c593eea
Michael Parisi <mparisi@organic.com>
2015-09-14 19:31:20 -0700

    working towards data structure

--- js/helpers.js
+++ js/helpers.js
@@ -3,3 +3,26 @@ if (!Date.now) {
         return +new Date();
     };
 }
+
+function getWebcamVideo(callback){
+    //Use webcam
+    var webcam = document.createElement('video');
+    webcam.width = 320;
+    webcam.height = 320;
+    webcam.autoplay = true;
+    webcam.loop = true;
+    //Webcam webcam
+    window.URL = window.URL || window.webkitURL;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    //get webcam
+    navigator.getUserMedia({
+        video: true
+    }, function(stream) {
+        //on webcam enabled
+        webcam.src = window.URL.createObjectURL(stream);
+        callback(webcam);
+    }, function(error) {
+        webcam = null;
+        callback(webcam);
+    });
+}
--- js/main.js
+++ js/main.js
@@ -1,28 +1,65 @@
 ;(function($){
     $(function(){
         
-        var USE_WEB_CAM = false;
         var container;
         var camera, scene, renderer, composer;
-        var uniforms = {};
-        var shaders = {};
-        var startTime = Date.now();
-        var time = 0;
         var lastPass = null;
         var webcam = null;
-        var webcamTexture = null;
-        var vs = $('#vs').text().trim();
-        var fs = $('#fs').text().trim();
-        var mouse = { x: 0.5, y: 0.5 };
-        var controls = null;
-        var gui = null;
-        var QUALITY = 1;
+        var webcamTexture = new THREE.Texture({ minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter });
 
-        var idCounter = 1;
+        var DEFAULT_VS = $('#vs').text().trim();
+        var DEFAULT_FS = $('#fs').text().trim();
+        var DEFAULT_UNIFORMS = {
+            time: { type: "f", value: 1.0 },
+            resolution: { type: "v2", value: new THREE.Vector2() },
+            mouse: { type: "v2", value: new THREE.Vector2() },
+            webcam: { type: "t", value: webcamTexture }
+        };
+        var MOUSE = { x: 0, y: 0 };
+        var WIDTH = 0;
+        var HEIGHT = 0;
+        var TIME = 0;
+        var START_TIME = Date.now();
+        var GUI = new dat.GUI();
+
+        var controls ={};
         var shaders = {};
+        var uniforms = {};
+
+        var defaultData = {
+            hideCode: false,
+            quality: 2,
+            webcam: false,
+
+            controls: [
+                {
+                    name: "Speed",
+                    uniform: {
+                        name: "speed",
+                        type: "f",
+                        value: 50.0
+                    },
+                    type: "slider",
+                    min: 0,
+                    max: 100
+                }
+            ],
+
+            shaders: [
+                {
+                    id: 0,
+                    name: "Default Shader",
+                    vs: DEFAULT_VS,
+                    fs: DEFAULT_FS,
+                    active: true
+                }
+            ]
+        };
+
+        var data = $.extend(true, defaultData, {});
 
         function init() {
-            container = document.getElementById( 'container' );
+            container = document.getElementById('container');
 
             camera = new THREE.Camera();
             camera.position.z = 1;
@@ -30,64 +67,62 @@
             scene = new THREE.Scene();
 
             renderer = new THREE.WebGLRenderer();
-            renderer.setPixelRatio( window.devicePixelRatio );
-            container.appendChild( renderer.domElement );
+            renderer.setPixelRatio(window.devicePixelRatio);
+            container.appendChild(renderer.domElement);
 
-            composer = new THREE.EffectComposer( renderer );
-            
-            uniforms = {
-                time: { type: "f", value: 1.0 },
-                resolution: { type: "v2", value: new THREE.Vector2() },
-                mouse: { type: "v2", value: new THREE.Vector2() }
-            };
+            composer = new THREE.EffectComposer(renderer);
 
-            if(webcam){
-                webcamTexture = new THREE.Texture( webcam );
-                webcamTexture.minFilter = THREE.LinearFilter;
-                webcamTexture.magFilter = THREE.LinearFilter;
-                uniforms.webcam = { type: "t", value: webcamTexture };
-            }
 
-            addShader();
-
-            onWindowResize();
-            window.addEventListener( 'resize', onWindowResize, false );
-
-            $(document).on('mousemove', function(event){
-                mouse.x = event.pageX/window.innerWidth;
-                mouse.y = event.pageY/window.innerHeight;
-            });
-
-            var Controls = function() {
-                this.quality = 1;
-                this.hideCode = false;
-            };
-
-            controls = new Controls();
-            gui = new dat.GUI();
-            var mainFolder = gui.addFolder('Main Controls');
-            var userFolder = gui.addFolder('User Controls');
+            var mainFolder = GUI.addFolder('Main Controls');
+            var userFolder = GUI.addFolder('User Controls');
             mainFolder.open();
             userFolder.open();
 
             var $codeMirrorContainer = $('#codemirror-container');
-            var hideCode = mainFolder.add(controls, 'hideCode');
+            var hideCode = mainFolder.add(data, 'hideCode').listen();
             hideCode.onFinishChange(function(value){
                 $codeMirrorContainer.toggleClass('disabled', value);
             });
-            var quality = mainFolder.add(controls, 'quality', [0.5, 1, 2, 4, 8]);
+            var quality = mainFolder.add(data, 'quality', [0.5, 1, 2, 4, 8]).listen();
             quality.onFinishChange(function(value){
-                QUALITY = value;
                 onWindowResize();
+            });
+
+            $.each(data.controls, function(index, control){
+                addControl(control);
+            });
+
+            $.each(data.shaders, function(index, shader){
+                addShader(shader);
+            });
+
+            onWindowResize();
+            window.addEventListener( 'resize', onWindowResize, false );
+            $(document).on('mousemove', function(event){
+                MOUSE.x = event.pageX/window.innerWidth;
+                MOUSE.y = event.pageY/window.innerHeight;
             });
         }
 
-        function addShader(){
-            var $tabButton = $('<button id="shader-tab-button--'+idCounter+'" class="shader-tab-button" data-shader-id="'+idCounter+'">Shader #'+idCounter+'</button>');
+        function addControl(control){
+            uniforms[control.uniform.name] = { type: control.uniform.type, value: control.uniform.value };
+
+            switch(control.type){
+                case "checkbox":
+                break;
+                case "dropdown":
+                break;
+                case "slider":
+                break;
+            }
+        }
+
+        function addShader(shader){
+            var $tabButton = $('<button id="shader-tab-button--'+shader.id+'" class="shader-tab-button" data-shader-id="'+shader.id+'">'+shader.name+'</button>');
             $tabButton.appendTo($('#shader-tab-buttons'));
             $tabButton.click(function(){ activateTab($(this).data('shader-id')); });
 
-            var $textArea = $('<textarea id="shader-tab-pane--'+idCounter+'" class="shader-tab-pane"></textarea>');
+            var $textArea = $('<textarea id="shader-tab-pane--'+shader.id+'" class="shader-tab-pane"></textarea>');
             $textArea.text(fs);
             $textArea.appendTo($('#shader-tab-panes'));
 
@@ -102,15 +137,6 @@
             composer.addPass(pass);
             lastPass = composer.passes[composer.passes.length-1];
             lastPass.renderToScreen = true;
-
-            shaders[idCounter] = {
-                'id': idCounter,
-                'editor': editor,
-                '$editor': $(editor.getWrapperElement()),
-                '$tabButton': $tabButton,
-                '$textArea': $textArea,
-                'pass': pass
-            };
 
             editor.on('change', function(){
                 pass.material.fragmentShader = editor.getValue();
@@ -132,55 +158,33 @@
             });
         }
 
-        function onWindowResize( event ) {
-            composer.setSize( window.innerWidth / QUALITY, window.innerHeight / QUALITY );
+        function onWindowResize(event) {
+            WIDTH = window.innerWidth / data.quality;
+            HEIGHT = window.innerHeight / data.quality;
+            composer.setSize(WIDTH, HEIGHT);
         }
 
         function animate() {
-            requestAnimationFrame( animate );
+            requestAnimationFrame(animate);
             render();
         }
 
         function render() {
             updateUniforms();
-            composer.render( scene, camera );
+            composer.render(scene, camera);
         }
         
         function updateUniforms(){
-            time = ( Date.now() - startTime ) / 1000;
-            if ( webcam && webcam.readyState === webcam.HAVE_ENOUGH_DATA && webcamTexture ){ webcamTexture.needsUpdate = true; }
+            TIME = (Date.now() - START_TIME) / 1000;
+            if (webcam && webcam.readyState === webcam.HAVE_ENOUGH_DATA){ webcamTexture.needsUpdate = true; }
 
-            $.each(composer.passes, function( index, pass ){
-                if(webcam && webcamTexture){ pass.uniforms.webcam.value = webcamTexture; }
-                pass.uniforms.time.value = time;
-                pass.uniforms.resolution.value.x = window.innerWidth / QUALITY;
-                pass.uniforms.resolution.value.y = window.innerHeight / QUALITY;
-                pass.uniforms.mouse.value.x = mouse.x;
-                pass.uniforms.mouse.value.y = mouse.y;
-            });
-        }
-
-        function getWebcamVideo(callback){
-            //Use webcam
-            webcam = document.createElement('video');
-            webcam.width = 320;
-            webcam.height = 320;
-            webcam.autoplay = true;
-            webcam.loop = true;
-            //Webcam webcam
-            window.URL = window.URL || window.webkitURL;
-            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-            //get webcam
-            navigator.getUserMedia({
-                video: true
-            }, function(stream) {
-                //on webcam enabled
-                webcam.src = window.URL.createObjectURL(stream);
-                callback();
-            }, function(error) {
-                prompt.innerHTML = 'Unable to capture WebCam. Please reload the page.';
-                webcam = null;
-                callback();
+            $.each(composer.passes, function(index, pass){
+                pass.uniforms.webcam.value = webcamTexture;
+                pass.uniforms.time.value = TIME;
+                pass.uniforms.resolution.value.x = WIDTH;
+                pass.uniforms.resolution.value.y = HEIGHT;
+                pass.uniforms.mouse.value.x = MOUSE.x;
+                pass.uniforms.mouse.value.y = MOUSE.y;
             });
         }
 
